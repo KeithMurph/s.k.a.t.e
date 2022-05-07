85bfcc2136f18afc143a9d059006e9d1fc5068e5
KeithMurph <keithmurphy94@gmail.com>
2022-05-06 23:43:37 -0700

    added 1 function to remove needless nested if statements

--- game.py
+++ game.py
@@ -3,11 +3,32 @@ from time import sleep
 from colorama import init, Fore, Back, Style
 
 # tricklist
-trickDict = {'ol': 'ollie', 'kf': 'kickflip', 'hf': 'heelflip', 'bssh': 'backside shuvit', 'fssh': 'frontside shuvit', 'tre': 'treflip', 'fs180': 'frontside 180', 'bs180': 'backside 180' }
+trickDict = {'ol': 'ollie', 'kf': 'kickflip', 'hf': 'heelflip', 'bssh': 'backside shuvit', 'fssh': 'frontside shuvit', 'fs180': 'frontside 180', 'bs180': 'backside 180', 'tre': 'treflip'}
 odds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100]
 skate = []
 game_active = True
 
+
+# functions 
+def checkLetter():
+    if skate == []:
+        skate.append('S')
+        print(skate)
+    elif skate == ['S']:
+        skate.append('K')
+        print(skate)    
+    elif skate == ['S', 'K']:
+        skate.append('A')
+        print(skate)
+    elif skate == ['S', 'K', 'A']:
+        skate.append('T')
+        print(skate)
+    elif skate == ['S', 'K', 'A', 'T']:
+        skate.append('E')
+        print('You have lost ☠️')
+        print(skate)
+        game_active = False
+            
 
 # Introduction to the game
 name = input(Fore.YELLOW + "What is your name? ")
@@ -42,23 +63,7 @@ while game_active == True:
 
         else:
             print(Fore.RED + 'slipped and fell!')
-            if skate == []:
-                skate.append('S')
-                print(skate)
-            elif skate == ['S']:
-                skate.append('K')
-                print(skate)
-            elif skate == ['S', 'K']:
-                skate.append('A')
-                print(skate)
-            elif skate == ['S', 'K', 'A']:
-                skate.append('T')
-                print(skate)
-            elif skate == ['S', 'K', 'A', 'T']:
-                skate.append('E')
-                print(skate)
-                print('You Lost!')
-                game_active = False
+            checkLetter()  
 
 # kickflip
     elif trickSelection == 'kf':
@@ -67,24 +72,7 @@ while game_active == True:
 
         else:
             print(Fore.RED + 'rolled ankle!')
-            if skate == []:
-                skate.append('S')
-                print(skate)
-            elif skate == ['S']:
-                skate.append('K')
-                print(skate)
-            elif skate == ['S', 'K']:
-                skate.append('A')
-                print(skate)
-            elif skate == ['S', 'K', 'A']:
-                skate.append('T')
-                print(skate)
-            elif skate == ['S', 'K', 'A', 'T']:
-                skate.append('E')
-                print(skate)
-                print('You Lost!')
-                game_active = False
-
+            checkLetter()
 
 # heelflip
     elif trickSelection == 'hf':
@@ -93,23 +81,7 @@ while game_active == True:
 
         else:
             print(Fore.RED + 'CREDIT CARD!')
-            if skate == []:
-                skate.append('S')
-                print(skate)
-            elif skate == ['S']:
-                skate.append('K')
-                print(skate)
-            elif skate == ['S', 'K']:
-                skate.append('A')
-                print(skate)
-            elif skate == ['S', 'K', 'A']:
-                skate.append('T')
-                print(skate)
-            elif skate == ['S', 'K', 'A', 'T']:
-                skate.append('E')
-                print(skate)
-                print('You Lost!')
-                game_active = False
+            checkLetter()
 
 
 # backside shuvit
@@ -119,23 +91,7 @@ while game_active == True:
 
         else:
             print(Fore.RED + 'SHINNER!')
-            if skate == []:
-                skate.append('S')
-                print(skate)
-            elif skate == ['S']:
-                skate.append('K')
-                print(skate)
-            elif skate == ['S', 'K']:
-                skate.append('A')
-                print(skate)
-            elif skate == ['S', 'K', 'A']:
-                skate.append('T')
-                print(skate)
-            elif skate == ['S', 'K', 'A', 'T']:
-                skate.append('E')
-                print(skate)
-                print('You Lost!')
-                game_active = False
+            checkLetter()
 
 
 # frontside shuvit
@@ -145,23 +101,7 @@ while game_active == True:
 
         else:
             print(Fore.RED + 'Fell on face!')
-            if skate == []:
-                skate.append('S')
-                print(skate)
-            elif skate == ['S']:
-                skate.append('K')
-                print(skate)
-            elif skate == ['S', 'K']:
-                skate.append('A')
-                print(skate)
-            elif skate == ['S', 'K', 'A']:
-                skate.append('T')
-                print(skate)
-            elif skate == ['S', 'K', 'A', 'T']:
-                skate.append('E')
-                print(skate)
-                print('You Lost!')
-                game_active = False     
+            checkLetter()   
 
 
 # frontside 180
@@ -170,24 +110,7 @@ while game_active == True:
             print(Fore.GREEN + 'Frontside 180! 🛹')
 
         else:
-            print(Fore.RED + 'Fell on hip!')
-            if skate == []:
-                skate.append('S')
-                print(skate)
-            elif skate == ['S']:
-                skate.append('K')
-                print(skate)
-            elif skate == ['S', 'K']:
-                skate.append('A')
-                print(skate)
-            elif skate == ['S', 'K', 'A']:
-                skate.append('T')
-                print(skate)
-            elif skate == ['S', 'K', 'A', 'T']:
-                skate.append('E')
-                print(skate)
-                print('You Lost!')
-                game_active = False                              
+            checkLetter()                             
 
 
 # backside 180
@@ -197,23 +120,7 @@ while game_active == True:
 
         else:
             print(Fore.RED + 'Fell on face!')
-            if skate == []:
-                skate.append('S')
-                print(skate)
-            elif skate == ['S']:
-                skate.append('K')
-                print(skate)
-            elif skate == ['S', 'K']:
-                skate.append('A')
-                print(skate)
-            elif skate == ['S', 'K', 'A']:
-                skate.append('T')
-                print(skate)
-            elif skate == ['S', 'K', 'A', 'T']:
-                skate.append('E')
-                print(skate)
-                print('You Lost!')
-                game_active = False   
+            checkLetter() 
 
 # treflip
     elif trickSelection == 'tre':
@@ -222,23 +129,7 @@ while game_active == True:
 
         else:
             print(Fore.RED + 'ATE IT!')
-            if skate == []:
-                skate.append('S')
-                print(skate)
-            elif skate == ['S']:
-                skate.append('K')
-                print(skate)
-            elif skate == ['S', 'K']:
-                skate.append('A')
-                print(skate)
-            elif skate == ['S', 'K', 'A']:
-                skate.append('T')
-                print(skate)
-            elif skate == ['S', 'K', 'A', 'T']:
-                skate.append('E')
-                print(skate)
-                print('You Lost!')
-                game_active = False   
+            checkLetter() 
     else:
         print('Invalid Input')
         print(trickDict)
