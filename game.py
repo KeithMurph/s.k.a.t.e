158bbdcbec11c22b3b1487f716b394dfcb293354
KeithMurph <keithmurphy94@gmail.com>
2022-05-07 22:14:27 -0700

    cleaned up code a bit

--- game.py
+++ game.py
@@ -6,8 +6,18 @@ from colorama import init, Fore, Back, Style
 trickDict = {'ol': 'OLLIE', 'kf': 'KICKFLIP', 'hf': 'HEELFLIP', 'bssh': 'BS SHUV', 'fssh': 'FS SHUV', 'fs180': 'FS 180', 'bs180': 'BS 180', 'tre': 'TREFLIP'}
 odds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100]
 skate = []
+usedTricks = []
 game_active = True
 bails = ('rolled ankle!', 'creditcard!', 'shinner!', 'fell on face', 'broken board!', 'hit a rock!')
+
+# function to see if trick in used tricks
+# def checkTrick():
+    # if currentTrick not in usedTricks:
+        # usedTricks.append(currentTrick)
+        # print('Used Tricks:', usedTricks)
+
+# function to pick current trick
+
 
 # function to check current letter
 def checkLetter():
@@ -31,7 +41,7 @@ def checkLetter():
 # Introduction to the game
 name = input(Fore.YELLOW + "What is your name? ")
 sleep(.5)
-print(Fore.CYAN + f"Hello {Fore.GREEN + name}", Fore.CYAN + "Welcome to the game of SKATE")
+print(Fore.CYAN + f"Hello {Fore.GREEN + name}", Fore.CYAN + "Welcome to the game of S.K.A.T.E")
 sleep(2)
 print(Fore.YELLOW + "The rules are simple match the computer's tricks to win!")
 print(Fore.WHITE, trickDict)
@@ -39,7 +49,10 @@ sleep(3)
 
 # Game loop
 while game_active == True:
+    # generate random trick
     currentTrick = random.choice(list(trickDict.values()))
+    # check if trick is already used
+  
     
     sleep(1)
     print(name,"'s score:" , Fore.GREEN, skate)
