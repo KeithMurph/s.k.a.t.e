a9d289858f3d780dd5033b30e42e6d416aea49d6
Keith Murphy <keithmurphy.dev@gmail.com>
2022-05-16 23:38:36 -0700

    py in html

--- game.py
+++ game.py
@@ -15,11 +15,8 @@ bails = (' rolled their ankle!', ' creditcard themselves!', ' got a shinner!', '
 streak = 6
 # function to check used tricks
 
-<<<<<<< HEAD
-=======
 # functions trick makes and bails
 # TODO
->>>>>>> 6a54325f274eb5a15a99c7f4a5c83d5b00b4fab0
 #  add turn based system
 #  cpu vs player turn
 #  cpu needs own stats for tricks
@@ -27,8 +24,6 @@ streak = 6
 #  if trick is successful proceed
 #  if unsuccessful switch turn to player
 #  if player turn ask for trick
-<<<<<<< HEAD
-=======
 #  if trick is successful proceed
 
 
@@ -45,7 +40,6 @@ def trickBail():
     print(name + Fore.RED + random.choice(bails))
     print()
 
->>>>>>> 6a54325f274eb5a15a99c7f4a5c83d5b00b4fab0
 
 # function to check current letter
 def checkLetter():
